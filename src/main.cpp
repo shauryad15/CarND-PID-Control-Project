a9f092f1db22321750627f9265c48b61788493ab
Shaurya Dwivedi <shaurya.dwivedi@ad.infosys.com>
2018-04-19 12:14:07 +0530

    Final values

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.20,0.0,0.45);
+  pid.Init(0.15, 0.0, 2.5);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -67,7 +67,7 @@ int main()
           json msgJson;
           msgJson["steering_angle"] = steer_value;
           
-		  msgJson["throttle"] = 0.05;
+		  msgJson["throttle"] = 0.3;
 		  
 		  
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
